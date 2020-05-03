bfe3bebcc971788261642724a92a2a4a0d13c81d
joaosavi <64634342+joaosavi@users.noreply.github.com>
2020-05-03 04:09:35 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,14 @@
 from iqoptionapi.stable_api import IQ_Option
 import time, json, logging, configparser
+from threading import Thread
 from datetime import datetime, timedelta
 from dateutil import tz
-
-
 
 def configuracao():  #Função para importar configurações
 	arquivo = configparser.RawConfigParser()
 	arquivo.read('config.txt')	
 	
-	return {'login': arquivo.get('GERAL', 'login'), 'senha': arquivo.get('GERAL', 'senha'), 'payout_min': arquivo.get('GERAL', 'payout_min'),'conta': arquivo.get('GERAL', 'conta'), 'valor_entrada': arquivo.get('GERAL', 'valor_entrada'), 'tempo': arquivo.get('GERAL', 'tempo')}
+	return {'login': arquivo.get('GERAL', 'login'), 'senha': arquivo.get('GERAL', 'senha'), 'payout_min': arquivo.get('GERAL', 'payout_min'),'conta': arquivo.get('GERAL', 'conta'), 'valor_entrada': arquivo.get('GERAL', 'valor_entrada'), 'tempo': arquivo.get('GERAL', 'tempo'), 'delay': arquivo.get('GERAL', 'delay'), 'fator_gale': arquivo.get('GERAL', 'fator_gale')}
 
 
 config = configuracao() 
@@ -79,24 +78,41 @@ def conferepar(atv):
                 continue
     return atvop
 
+def operacao_thread ():
+    status,id = API.buy_digital_spot(ativo_sinal,entrada_sinal,direcao_sinal,tempo_sinal)     
+    #Print do resultado
+    if isinstance(id, int):
+        while True:
+            status,lucro = API.check_win_digital_v2(id)
+            if status:  
+                if lucro > 0:
+                    resultado_operacao = str('RESULTADO: WIN / LUCRO: '+str(round(lucro, 2)))
+                    print(resultado_operacao)
+                else:
+                    resultado_operacao = str('RESULTADO: LOSS / LUCRO: -'+str(entrada_sinal))
+                    print(resultado_operacao)
+                    entrada_sinal_gale = int(entrada_sinal) * int(config['fator_gale'])
+                    status,id = API.buy_digital_spot(ativo_sinal,entrada_sinal_gale,direcao_sinal,tempo_sinal)
+                    if isinstance(id, int):
+                        while True:
+                            status,lucro = API.check_win_digital_v2(id)
+                            if status:  
+                                if lucro > 0:
+                                    resultado_operacao = str('RESULTADO GALE: WIN / LUCRO: '+str(round(lucro, 2)))
+                                    print(resultado_operacao)
+                                else:
+                                    resultado_operacao = str('RESULTADO GALE: LOSS / LUCRO: -'+str(entrada_sinal))
+                                    print(resultado_operacao)
+                                break
+                break           
+    return
+
 
 
 x = perfil() 
 print('Nome: ',x['name'],'\n')
 print('Saldo:',round(x['balance'], 2),x['currency'],'\n') 
 print('\n\n')
-
-
-
-'''
-par = API.get_all_open_time()
-for paridade in par['digital']:
-	if par['digital'][paridade]['open'] == True:
-		print('[ DIGITAL ]: '+paridade+' | Payout: '+str( payout(paridade, 'digital') ))
-
-
-conferepar(dados[1]) == True and 
-'''
 
 
 lista = carregar_sinais()
@@ -112,87 +128,38 @@ for sinal in lista:
     entrada_sinal = float(config['valor_entrada'])
     direcao_sinal = dados[2]
     tempo_sinal = int(config['tempo'])
+#def robocaralho():
     if str(payout(dados[1],'digital')) > str(config['payout_min']) and conferepar(ativo_sinal) == True:
         print('CONDIÇOES SATISFEITAS')
         print('ESPERANDO PARA ENTRAR NA OPERAÇAO')
         print('\n')
         while True:
-            d = datetime.now() - timedelta(seconds=1)
+            delay = int(config['delay'])
+            d = datetime.now() + timedelta(seconds=int(delay))
             datual = d.strftime('%Y-%m-%d %H:%M:%S')
             time.sleep(0.100)
+
             if datual == dados[0]:
                 print('ENTROU NA OPERAÇAO')
                 print('\n')
-                status,id = API.buy_digital_spot(ativo_sinal,entrada_sinal,direcao_sinal,tempo_sinal) 
-                '''          
-                #Print do resultado
-                if isinstance(id, int):
-                    while True:
-                        status,lucro = API.check_win_digital_v2(id)
-                        if status:  
-                            if lucro > 0:
-                                print('RESULTADO: WIN / LUCRO: '+str(round(lucro, 2)))
-                            else:
-                                print('RESULTADO: LOSS / LUCRO: -'+str(entrada_sinal))
-                            break
-                        '''                      
-                break
+                t1 = Thread(target= operacao_thread,args=[])
+                t1.start()
+                break                
             elif datual > dados[0]:
                 print('Tempo expirado')
-                print('\n')
                 break
     else:
         print('ativo sem condiçoes')
         print('\n')
-
-'''
-while True:
-    datual = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    print(datual)
-    time.sleep(1)
-    if datual == dados[0]:
-
-
-            if par['digital'][paridade]['open'] == True and str(payout(paridade,'digital')) > str(config['payout_min']):
-                print('entrou')
-                status,id = API.buy_digital_spot(dados[1],config['valor_entrada'],dados[2],config['tempo'])
-        break
-    
- '''   
-
-            
- 
-'''
-print('ATIVOS ABERTOS:')
-
-
-
-
-
-
-ativo = 'EURUSD-OTC'
-valor_entrada = 2
-direcao = 'call'
-tempo = 1
-if ativo == dados[1]:
-    status,id = API.buy_digital_spot(ativo, valor_entrada, direcao, tempo)  #Status retorna false ou true e id é o id da operação
-
-    
-#Print do resultado
-if isinstance(id, int):
-	while True:
-		status,lucro = API.check_win_digital_v2(id)   
-		
-		if status:  
-			if lucro > 0:
-				print('RESULTADO: WIN / LUCRO: '+str(round(lucro, 2)))
-			else:
-				print('RESULTADO: LOSS / LUCRO: -'+str(valor_entrada))
-			break
-
-
-print('Saldo:',saldo_atualizado,x['currency'],'\n') 
-'''
-
+    #return
+#t2 = Thread(target= robocaralho, args= [])
+#t2.start()
 
 print('ROBO DESLIGADO!')
+
+
+''' 
+d = datetime.now() - timedelta(seconds=3)
+datual = d.strftime('%Y-%m-%d %H:%M:%S')
+time.sleep(0.100)
+''' 

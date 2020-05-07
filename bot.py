2c4eef4bb0ee8b14427cc1f7e2b6e47a5664534e
joaosavi <64634342+joaosavi@users.noreply.github.com>
2020-05-07 00:20:22 -0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,25 +4,64 @@ from threading import Thread
 from datetime import datetime, timedelta
 from dateutil import tz
 
+testedelay = input('Quer iniciar teste de delay? y/n\n')
 
-'''
-print('APERTE ENTER PARA SE CONECTAR')   
-input()
-'''
-
-
+#Tem que ser loadado primeiro
 def configuracao():  #Função para importar configurações
 	arquivo = configparser.RawConfigParser()
 	arquivo.read('config.txt')	
 	
-	return {'login': arquivo.get('GERAL', 'login'), 'senha': arquivo.get('GERAL', 'senha'), 'payout_min': arquivo.get('GERAL', 'payout_min'),'conta': arquivo.get('GERAL', 'conta'), 'valor_entrada': arquivo.get('GERAL', 'valor_entrada'), 'tempo': arquivo.get('GERAL', 'tempo'), 'delay': arquivo.get('GERAL', 'delay'), 'fator_gale': arquivo.get('GERAL', 'fator_gale')}
+	return {'login': arquivo.get('LOGIN', 'login'), 'senha': arquivo.get('LOGIN', 'senha'), 'payout_min': arquivo.get('ENTRADA', 'payout_min'),'conta': arquivo.get('LOGIN', 'conta'), 'valor_entrada': arquivo.get('ENTRADA', 'valor_entrada'), 'tempo': arquivo.get('ENTRADA', 'tempo'), 'delay': arquivo.get('DELAY', 'delay'), 'fator_gale': arquivo.get('ENTRADA', 'fator_gale'), 'ativo_base': arquivo.get('DELAY', 'ativo_base')}
+config = configuracao()
 
+if testedelay == 'y':
+    API = IQ_Option (config['login'],config['senha'])
+    API.connect()
+    API.change_balance('PRACTICE')
+    
+    d2 = datetime.now() + timedelta(minutes=1)
+    d3 = d2.replace(second=0)
+    datual2 = d3.strftime('%H:%M:%S')
 
-config = configuracao() 
+    while True:
+        delay = int(config['delay'])
+        d = datetime.now() + timedelta(seconds=delay)
+        datual = d.strftime('%H:%M:%S')
+        time.sleep(1)
+
+        tempo_sinal = datual2
+        ativo_sinal = config['ativo_base']
+        direcao_sinal = 'call'
+
+        if datual == tempo_sinal:
+            hora_sinal_entrada = tempo_sinal
+            ativo_sinal_entrada = ativo_sinal
+            valor_sinal_entrada = float(config['valor_entrada'])
+            direcao_sinal_entrada = direcao_sinal
+            tempo_sinal_entrada = int(config['tempo'])
+            print('\nAtivo: ' +str(ativo_sinal_entrada)+ '\nHora: ' +str(hora_sinal_entrada)+ '\nDireção: ' +str(direcao_sinal_entrada)+ '\n--------------------------------------------------------------------------------\n                              ENTROU NA OPERAÇAO\n--------------------------------------------------------------------------------')
+
+            status,id = API.buy_digital_spot(ativo_sinal_entrada,valor_sinal_entrada,direcao_sinal_entrada,tempo_sinal_entrada)  
+            exit()
+
+#Tem que ser loadado antes de request na API
 API = IQ_Option (config['login'],config['senha'])
 API.connect()
+API.change_balance(config['conta'])
+printdadosteste = True
 
-API.change_balance(config['conta']) #REAL, PRACTICE
+def carregar_sinais():  
+        arquivo = open('sinais.txt', encoding='UTF-8')
+        lista = arquivo.read()
+        arquivo.close
+
+        lista = lista.split('\n')
+
+        for index,a in enumerate(lista):
+            if a == '':
+                del lista[index]
+
+        return lista
 
 while True:   #Mensagem de conexão
     if API.check_connect() == False:
@@ -35,18 +74,18 @@ while True:   #Mensagem de conexão
     
     time.sleep(1)
     
-def perfil():  #Função para pegar dados da conta
+def perfil(): 
     perfil = json.loads(json.dumps(API.get_profile_ansyc()))
     
     return perfil
 
-def timestamp_converter(x): # Função para converter timestamp
+def timestamp_converter(x):  
 	hora = datetime.strptime(datetime.utcfromtimestamp(x).strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S')
 	hora = hora.replace(tzinfo=tz.gettz('GMT'))
 	
 	return str(hora.astimezone(tz.gettz('America/Sao Paulo')))[:-6]
 
-def payout(par, tipo,timeframe = 1):  #Função para converter payout
+def payout(par, tipo,timeframe = 1): 
 
     if tipo == 'digital':
     
@@ -60,19 +99,6 @@ def payout(par, tipo,timeframe = 1):  #Função para converter payout
         API.unsubscribe_strike_list(par, timeframe)
         return d
 
-def carregar_sinais():  #Função para carregar sinais
-    arquivo = open('sinais.txt', encoding='UTF-8')
-    lista = arquivo.read()
-    arquivo.close
-
-    lista = lista.split('\n')
-
-    for index,a in enumerate(lista):
-        if a == '':
-            del lista[index]
-
-    return lista
-
 def conferepar(atv):
     par = API.get_all_open_time()
     for paridade in par['digital']: 
@@ -85,18 +111,14 @@ def conferepar(atv):
                 continue
     return atvop
 
+#VARIAVEIS
+lista = carregar_sinais()
+x = perfil()
 
-x = perfil() 
+#PRINT DE DADOS 
 print('Nome: ',x['name'],'\n')
 print('Saldo:',round(x['balance'], 2),x['currency'],'\n') 
 print('\n\n')
-
-
-'''
-print('APERTE ENTER PARA CARREGAR SINAIS')   
-input()
-'''
-
 
 
 def sinais_thread ():
@@ -104,7 +126,8 @@ def sinais_thread ():
     ativo_sinal = dados[1]
     direcao_sinal = dados[2]
     if str(payout(dados[1],'digital')) > str(config['payout_min']) and conferepar(ativo_signal) == True:
-        print('\nAtivo: ' +str(ativo_sinal)+ '\nHora: ' +str(tempo_sinal)+ '\nDireção: ' +str(direcao_sinal)+ '\n--------------------------------------------------------------------------------\n                                 EM ESPERA\n--------------------------------------------------------------------------------')
+        if printdadosteste == True:
+            print('\nAtivo: ' +str(ativo_sinal)+ '\nHora: ' +str(tempo_sinal)+ '\nDireção: ' +str(direcao_sinal)+ '\n--------------------------------------------------------------------------------\n                                 EM ESPERA\n--------------------------------------------------------------------------------')
         while True:
             delay = int(config['delay'])
             d = datetime.now() + timedelta(seconds=int(delay))
@@ -117,7 +140,6 @@ def sinais_thread ():
                 valor_sinal_entrada = float(config['valor_entrada'])
                 direcao_sinal_entrada = direcao_sinal
                 tempo_sinal_entrada = int(config['tempo'])
-
                 print('\nAtivo: ' +str(ativo_sinal_entrada)+ '\nHora: ' +str(hora_sinal_entrada)+ '\nDireção: ' +str(direcao_sinal_entrada)+ '\n--------------------------------------------------------------------------------\n                              ENTROU NA OPERAÇAO\n--------------------------------------------------------------------------------')
 
                 status,id = API.buy_digital_spot(ativo_sinal_entrada,valor_sinal_entrada,direcao_sinal_entrada,tempo_sinal_entrada)  
@@ -145,17 +167,19 @@ def sinais_thread ():
                             break
                 break                
             elif datual > tempo_sinal:
-                print('--------------------------------------------------------------------------------\n                               SINAL EXPIRADO\n--------------------------------------------------------------------------------')
+                if printdadosteste == True:
+                    print('--------------------------------------------------------------------------------\n                               SINAL EXPIRADO\n--------------------------------------------------------------------------------')
                 break
     else:
         tempo_sinal = dados[0]
         ativo_sinal = dados[1]
         direcao_sinal = dados[2]
-        print('\nAtivo: ' +str(ativo_sinal)+ '\nHora: ' +str(tempo_sinal)+ '\nDireção: ' +str(direcao_sinal)+ '\n--------------------------------------------------------------------------------\n                               ATIVO NEGADO\n                         SEM CONDIÇOES DE ENTRADA\n--------------------------------------------------------------------------------')
+        if printdadosteste == True:
+            print('\nAtivo: ' +str(ativo_sinal)+ '\nHora: ' +str(tempo_sinal)+ '\nDireção: ' +str(direcao_sinal)+ '\n--------------------------------------------------------------------------------\n                               ATIVO NEGADO\n                         SEM CONDIÇOES DE ENTRADA\n--------------------------------------------------------------------------------')
     return
 
 
-lista = carregar_sinais()
+
 for sinal in lista:
     dados = sinal.split(',')
   
